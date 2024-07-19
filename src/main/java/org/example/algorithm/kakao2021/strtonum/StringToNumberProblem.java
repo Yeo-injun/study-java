429a96b557a4e2a4db8a796f0b0b147dc9ae4ea0
Yeo-injun <youth.com11@gmail.com>
2024-07-19 22:50:22 +0900

    [ Chore ] StringToNumberProblem 소스코드 정리

--- src/main/java/org/example/algorithm/kakao2021/strtonum/StringToNumberProblem.java
+++ src/main/java/org/example/algorithm/kakao2021/strtonum/StringToNumberProblem.java
@@ -10,20 +10,21 @@ public class StringToNumberProblem implements StudyLauncher {
     private static final String PROBLEM_URL = "https://school.programmers.co.kr/learn/courses/30/lessons/81301";
     @Override
     public void launch() {
-        TestTarget[] targets = {
-            new Target( "one4seveneight", 1478 ),
-            new Target( "23four5six7", 234567 ),
-            new Target( "2three45sixseven", 234567 ),
-            new Target( "123", 123 ),
-        };
-        AlgorithmTester tester = new AlgorithmTester( Arrays.asList( targets ) );
+        AlgorithmTester tester = new AlgorithmTester(
+            Arrays.asList( new Target[] {
+                new Target( "one4seveneight", 1478 ),
+                new Target( "23four5six7", 234567 ),
+                new Target( "2three45sixseven", 234567 ),
+                new Target( "123", 123 ),
+            })
+        );
         tester.test( new StringToNumberSolution01() );
     }
 
     class Target implements TestTarget<String, Integer> {
 
-        private String input;
-        private Integer expected;
+        private final String input;
+        private final Integer expected;
 
         public Target( String input, Integer expected ) {
             this.input      = input;

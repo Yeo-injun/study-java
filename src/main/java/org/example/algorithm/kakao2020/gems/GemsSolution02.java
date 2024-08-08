a8a1f561c2ea753ec8cc8e6074fa8eea5e35cf06
Yeo-injun <youth.com11@gmail.com>
2024-08-08 21:39:37 +0900

    [ Feat ] GemsProblem Sol02 추가 - 조기종료조건 추가하여 효율성 테스트 5/15 Pass

--- src/main/java/org/example/algorithm/kakao2020/gems/GemsSolution02.java
+++ src/main/java/org/example/algorithm/kakao2020/gems/GemsSolution02.java
@@ -5,57 +5,61 @@ import org.example.algorithm.Solver;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class GemsSolution01 implements Solver<int[], String[]> {
+public class GemsSolution02 implements Solver<int[], String[]> {
 
-    /**
-     * 정확성 테스트만 통과
-     * - 효율성 테스트 Fail
-     */
     @Override
     public int[] solve( String[] gems ) {
         // 보석종류 추출하기
-        Set<String> gemTypes = Arrays.stream( gems ).collect( Collectors.toSet() );
-        Map<String, Integer> gemsIdxMap = initGemsIdxMap( gemTypes );
+        Map<String, Integer> gemsIdxMap = initGemsIdxMap( gems );
         int startIdx    = 0;
         int lastIdx     = gems.length;
-        int lastStartIdx = lastIdx - gemTypes.size();
+        int lastStartIdx = lastIdx - gemsIdxMap.size();
         int minPart = Integer.MAX_VALUE;
         int[] answer = new int[] { 0, 0 };
         while ( startIdx <= lastStartIdx ) {
 
             // 시작지점보다 뒤쳐진 인덱스의 보석은 인덱스 업데이트
-
-            Set<String> targetForUpdateIdx = new HashSet<>();
-            for ( String gem : gemTypes ) {
-                int gemIdx = gemsIdxMap.get( gem );
-                if ( gemIdx < startIdx ) {
-                    targetForUpdateIdx.add( gem );
+            Set<String> updateTarget = new HashSet<>();
+            final int baseIdx = startIdx;
+            gemsIdxMap.forEach(( g, idx ) -> {
+                int gemIdx = gemsIdxMap.get( g );
+                if ( gemIdx < baseIdx ) {
+                    updateTarget.add( g );
                 }
-            }
+            });
 
             // 인덱스를 업데이트해야 하는 보석들의 인덱스 업데이트
+
             for ( int i = startIdx; i <= lastIdx-1; i++ ) {
                 String newGem = gems[ i ];
-                if ( targetForUpdateIdx.remove( newGem ) ) {
+                if ( updateTarget.remove( newGem ) ) {
                     gemsIdxMap.put( newGem, i );
+                }
+                if ( updateTarget.isEmpty() ) {
+                    break;
                 }
             }
 
-            if ( !targetForUpdateIdx.isEmpty() ) {
+            if ( !updateTarget.isEmpty() ) {
                 startIdx++;
                 continue;
             }
 
-            Collection<Integer> gemsIdxValue = gemsIdxMap.values();
-            int gemLastIdx = startIdx;
-            for ( int idx : gemsIdxValue ) {
-                gemLastIdx = Math.max( gemLastIdx, idx );
-            }
 
+            // 자연정렬로 정렬 >> 숫자는 오름찬순 / 문자열은 사전순
+            // 역순으로 정렬하여 맨처음 값 가져오기
+            List<Integer> idxList = gemsIdxMap.values()
+                                        .stream()
+                                        .sorted( Comparator.reverseOrder() )
+                                        .collect( Collectors.toList() );
+            int gemLastIdx = idxList.get( 0 );
             if ( minPart > ( gemLastIdx - startIdx ) ) {
                 minPart = gemLastIdx - startIdx;
                 answer[0] = startIdx;
                 answer[1] = gemLastIdx;
+                if ( minPart == gems.length ) {
+                    break;
+                }
             }
             // 마지막 초기화
             startIdx++;
@@ -65,9 +69,9 @@ public class GemsSolution01 implements Solver<int[], String[]> {
         return answer;
     }
 
-    private Map<String, Integer> initGemsIdxMap( Set<String> gemTypes ) {
+    private Map<String, Integer> initGemsIdxMap( String[] gems ) {
         Map<String, Integer> gemsIdx = new HashMap<>();
-        for ( String gemType : gemTypes ) {
+        for ( String gemType : gems ) {
             gemsIdx.put( gemType, -1 );
         }
         return gemsIdx;

dc6ddb90e1e152c7f35ae955eedc9091d2f58cdb
Yeo-injun <youth.com11@gmail.com>
2024-08-09 00:41:13 +0900

    [ Feat ] GemsProblem Sol02 추가 - 조기종료조건 추가하여 효율성 테스트 7/15 Pass

--- src/main/java/org/example/algorithm/kakao2020/gems/GemsSolution02.java
+++ src/main/java/org/example/algorithm/kakao2020/gems/GemsSolution02.java
@@ -9,7 +9,6 @@ public class GemsSolution02 implements Solver<int[], String[]> {
 
     @Override
     public int[] solve( String[] gems ) {
-        // 보석종류 추출하기
         Map<String, Integer> gemsIdxMap = initGemsIdxMap( gems );
         int startIdx    = 0;
         int lastIdx     = gems.length;
@@ -17,18 +16,17 @@ public class GemsSolution02 implements Solver<int[], String[]> {
         int minPart = Integer.MAX_VALUE;
         int[] answer = new int[] { 0, 0 };
         while ( startIdx <= lastStartIdx ) {
-
-            // 시작지점보다 뒤쳐진 인덱스의 보석은 인덱스 업데이트
+            int baseIdx = startIdx;
             Set<String> updateTarget = new HashSet<>();
-            final int baseIdx = startIdx;
-            gemsIdxMap.forEach(( g, idx ) -> {
-                int gemIdx = gemsIdxMap.get( g );
-                if ( gemIdx < baseIdx ) {
+            gemsIdxMap.forEach( ( g, i ) -> {
+                if ( i < baseIdx ) {
                     updateTarget.add( g );
                 }
             });
 
-            // 인덱스를 업데이트해야 하는 보석들의 인덱스 업데이트
+            if ( updateTarget.isEmpty() ) {
+                break;
+            }
 
             for ( int i = startIdx; i <= lastIdx-1; i++ ) {
                 String newGem = gems[ i ];
@@ -45,21 +43,19 @@ public class GemsSolution02 implements Solver<int[], String[]> {
                 continue;
             }
 
-
-            // 자연정렬로 정렬 >> 숫자는 오름찬순 / 문자열은 사전순
-            // 역순으로 정렬하여 맨처음 값 가져오기
-            List<Integer> idxList = gemsIdxMap.values()
-                                        .stream()
-                                        .sorted( Comparator.reverseOrder() )
-                                        .collect( Collectors.toList() );
-            int gemLastIdx = idxList.get( 0 );
+            Collection<Integer> gemsIdxValue = gemsIdxMap.values();
+            int gemLastIdx = startIdx;
+            for ( int idx : gemsIdxValue ) {
+                gemLastIdx = Math.max( gemLastIdx, idx );
+            }
             if ( minPart > ( gemLastIdx - startIdx ) ) {
                 minPart = gemLastIdx - startIdx;
                 answer[0] = startIdx;
                 answer[1] = gemLastIdx;
-                if ( minPart == gems.length ) {
-                    break;
-                }
+            }
+            // 중요한 예외 케이스
+            if ( minPart == gems.length ) {
+                break;
             }
             // 마지막 초기화
             startIdx++;
